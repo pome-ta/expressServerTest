0b9f61d5efd76339cc38c0f003b90eb9dab48949
pome-ta <pometarou93@gmail.com>
2021-04-19 16:29:37 +0900

    log に出すものを増やした

--- index.js
+++ index.js
@@ -6,12 +6,19 @@ const app = express();
 const port = process.env['WEB_APP_PORT'];
 
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) =>{
+  res.send("Hello, world! 😊");
+  console.log("req ---");
+  console.log({req});
+  console.log("res ---");
+  console.log({res});
+});
 
 // Launch app
 app.listen(port, () => {
   console.log(
-    'Launching app... http://localhost:' + port + '\n'
+    `Launching app... http://localhost:${port}` + '\n'
   );
+  console.log({app});
 });
 
