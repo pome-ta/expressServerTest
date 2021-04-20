a603e56c1118b9b971f8b9aae785e26c4dc71c34
pome-ta <pometarou93@gmail.com>
2021-04-20 13:17:47 +0900

    画像表示パラメータ

--- index.js
+++ index.js
@@ -10,15 +10,17 @@ app.get('/', (req, res) => {
   // memo: どっちの呼び方でも大丈夫だった
   //res.sendFile('/index.html', { root: __dirname });
   res.sendFile(`${__dirname}/index.html`);
-  console.log("req ---");
-  console.log({req});
-  console.log("res ---");
-  console.log({res});
+  console.log('アクセス: /');
+});
+app.get('/images/:file', (req, res) => {
+  const file = req.params.file;
+  res.sendFile(`${__dirname}/images/${file}`);
+  console.log('アクセス: /images');
 });
 
 // Launch app
 app.listen(port, () => {
   console.log(`Launching app... http://localhost:${port}` + '\n');
-  console.log({app});
+  //console.log({app});
 });
 
